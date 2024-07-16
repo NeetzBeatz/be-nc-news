eb6058b007b2667640efbf64085056aabc7a2646
NeetzBeatz <anitaleahcampbell@hotmail.co.uk>
2024-07-16 15:15:41 +0100

    MVC updated to pass tests and output articles by id

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,7 +1,7 @@
-const {fetchTopics, fetchArticlesByID} = require("../models/models");
+const {fetchTopics, fetchArticleByID} = require("../models/models");
 const endpoints = require("../endpoints.json")
 
-exports.getTopics = (request, response, next) => {
+exports.getTopics  = (request, response, next) => {
     fetchTopics().then((topics) => {
         response.status(200).send({topics : topics})
     }).catch((err) => {
@@ -12,4 +12,14 @@ exports.getTopics = (request, response, next) => {
 exports.getEndpoints = (request, response, next) => {
         return response.status(200).send({endpoints : endpoints});
 };
+
+exports.getArticleByID = (request, response, next) => {
+    const {article_id} = request.params
+        fetchArticleByID(article_id).then((article) => {
+            response.status(200).send({article : article})
+        })
+        .catch((err) => {
+            next(err);
+        })
+}
 
--- models/models.js
+++ models/models.js
@@ -6,3 +6,16 @@ exports.fetchTopics = (request, response, next) => {
     });
 };
 
+exports.fetchArticleByID = (article_id) => {
+    return db.query("SELECT * FROM articles WHERE article_id = $1", [article_id]).then(({rows})=> {
+        if(rows.length === 0){
+            return Promise.reject({
+                status : 404,
+                msg : `no article found with an article_id of ${article_id}`
+            })
+        }
+        return rows[0]
+    })
+}
+
+
